1bd301e1d93b1c26bf4bd5163a66e5c7055d9b1d
tomi <tomi.seregi99@gmail.com>
2026-09-10 15:58:05 +0200

    release: prepare 0.3.3 distribution

--- src/index.ts
+++ src/index.ts
@@ -97,7 +97,7 @@ function workerEnginesEnv(env: Env, db: Db): WorkerEnginesEnv {
 }
 
 // Keep in sync with package.json "version".
-const SERVER_VERSION = '0.3.2';
+const SERVER_VERSION = '0.3.3';
 
 export class GeoMcpAgent extends McpAgent<Env, unknown, AgentProps> {
   server = new McpServer({
